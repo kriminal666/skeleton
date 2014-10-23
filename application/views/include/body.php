28466dc0bf0cd78fe8f601ff3e6275f97eeb2435
kriminal666 <piltrafiya_666@hotmail.com>
2014-10-23 12:08:54 +0200

    Cambios para hacer commit: 	modificado: application/views/include/body.php
    Prueba del body

--- application/views/include/body.php
+++ application/views/include/body.php
@@ -1,3 +1,17 @@
 <div id="skeleton_body">
 <?php echo "PUT YOUR DEFAULT BODY CONTENT HERE (file: views/include/body.php) or edit function _load_body";?>
+<p><b>Insertar usuario</b></p>
+		
+		
+		<form id="form" action="#" method="POST">
+			<p>Nombre: <input name="name" id="name" type="text"></p>
+			<p>Apellido: <input name="lastname" id="lastname" type="text"></p>
+			<p>Twitter:  <input name="twitter" id="twitter" type="text"></p>
+			<p>Web:   <input name="web" id="twitter" type="text"></p>
+			<input type="submit" value="Guardar Usuario" id="save">
+		</form>
+
+
+
 </div>
+
